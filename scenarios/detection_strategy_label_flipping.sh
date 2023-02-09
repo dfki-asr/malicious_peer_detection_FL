e4385ee5b9010e7b5287155905ec709d2f2f454d
Melvin Chelli <melvin.chelli@dfki.de>
2023-02-09 14:21:45 +0000

    Increased number of clients

--- scenarios/detection_strategy_label_flipping.sh
+++ scenarios/detection_strategy_label_flipping.sh
@@ -11,4 +11,12 @@ python3 utils/partition_data.py --n_partitions 10 --dataset mnist
 python3 server.py --strategy $strategy --attack $attack &
 sleep 3
 python3 client.py --num 0 --attack 'none' &
-python3 client.py --num 1 --attack $attack
+python3 client.py --num 1 --attack 'none' &
+python3 client.py --num 2 --attack $attack &
+python3 client.py --num 3 --attack 'none'&
+python3 client.py --num 4 --attack $attack  &
+python3 client.py --num 5 --attack 'none' &
+python3 client.py --num 6 --attack $attack &
+python3 client.py --num 7 --attack $attack &
+python3 client.py --num 7 --attack $attack &
+python3 client.py --num 7 --attack 'none' 
